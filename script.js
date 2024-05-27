568eb8bf1eedd7ea7572c74b333769394efa9fc7
Rycardo Blanco <rycardoblanco@hotmail.com>
2024-05-27 10:19:43 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+class Estudante{
+    constructor(nome,email,ra,curso,dic){
+        this.nome = nome
+        this.email = email
+        this.ra = ra
+        this.curso = curso
+        this.dic = dic
+    }
+}
+function primeiraDiciplina(){
+
+}
+
+function ultimaDiciplina(){
+    
+}
